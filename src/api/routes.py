7be80e57317b5497ff2fb8b912177343f2fd4715
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-08-22 15:13:43 +0000

    com

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +22,52 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    request_body = request.json
+    user_query = User.query.filter_by(email=request_body['email']).first()
+    if user_query is None:
+        create_user = User(name=request_body['name'], email=request_body['email'],  password=request_body['password'], is_active=request_body['is_active'])
+        db.session.add(create_user)
+        db.session.commit()
+        response_body = {
+            "msg": "User created successfully"
+        }
+        return jsonify(response_body), 200
+    else:
+        response_body = {
+            "msg": "User already exists"
+        }
+        return jsonify(response_body), 404
+    
+
+@api.route('/login', methods=['POST'])
+def login_user():
+    request_body = request.json
+    email = request_body.get('email')
+    password = request_body.get('password')
+    user_login = User.query.filter_by(email=email).first()
+    if user_login is None:
+        response_body = {
+            "msg": "User does not exist"
+        }
+        return jsonify(response_body), 404
+    elif password != user_login.password:
+        response_body = {
+            "msg": "Incorrect password"
+        }
+        return jsonify(response_body), 404
+    else:
+        access_token = create_access_token(identity=user_login.id)
+        return jsonify({"token": access_token, "user_id": user_login.id}), 200
+
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Accede a la identidad del usuario actual con get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"id": user.id, "username": user.username }), 200
